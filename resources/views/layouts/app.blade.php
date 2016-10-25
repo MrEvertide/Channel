4b2ab63e2d6a372a365a8e22a857c870ae2ff402
Cédric Nolin <ced.star.002@gmail.com>
2016-10-24 23:33:05 -0400

    fix menu + avatar position

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,21 +42,15 @@
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ url('/login') }}">Login</a></li>
                             <li><a href="{{ url('/register') }}">Register</a></li>
                         @else
-                            <img src="/uploads/avatar/{{Auth::user()->picture}}" alt="profile picture" class="avatar-small">
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    <img src="/uploads/avatar/{{Auth::user()->picture}}" alt="profile picture" class="avatar-small">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
