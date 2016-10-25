55a36b94ca33cc9e5b70d1cbd8c248004b294607
Cédric Nolin <ced.star.002@gmail.com>
2016-10-24 22:01:24 -0400

    add profile image styling to the settings page + layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,6 +54,7 @@
                             <li><a href="{{ url('/login') }}">Login</a></li>
                             <li><a href="{{ url('/register') }}">Register</a></li>
                         @else
+                            <img src="/uploads/avatar/{{Auth::user()->picture}}" alt="profile picture" class="avatar-small">
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -4,18 +4,15 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{Auth::user()->name}}'s profile</div>
-                <div class="panel-body">
-                    <img src="/uploads/avatar/{{Auth::user()->picture}}" alt="">
-
-                    <form enctype="multipart/form-data" action="/settings" method="POST">
-                        <label for="">Update profile image</label>
-                        <input type="file" name="avatar">
-                        <input type="hidden" name="_token" value="{{csrf_token()}}">
-                        <button type="submit" class="pull-right btn btn-sm btn-primary">Upload picture</button>
-                    </form>
-                </div>
+            <img src="/uploads/avatar/{{Auth::user()->picture}}" alt="profile picture" class="settings-avatar-image">
+            <div class="profile-image">
+                <h2>{{Auth::user()->name}}'s profile</h2>
+                <form enctype="multipart/form-data" action="/settings" method="POST" class="avatar-form">
+                    <label for="avatar">Change your avatar</label>
+                    <input type="file" name="avatar">
+                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                    <button type="submit" class="pull-right btn btn-sm btn-primary">Upload picture</button>
+                </form>
             </div>
         </div>
     </div>
