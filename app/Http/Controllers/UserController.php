29b688ab42fdea9b803dbd709d50e92f2996eafa
Cédric Nolin <ced.star.002@gmail.com>
2016-10-24 21:32:08 -0400

    add method to handle avatar upload, gitignore the folder for user files

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use Image;
 
 
 class UserController extends Controller
@@ -28,6 +29,16 @@ class UserController extends Controller
     }
 
     public function update_avatar(Request $request) {
+        if ($request->hasFile('avatar')) {
+            $avatar = $request->file('avatar');
+            $filename = time() . '.' . $avatar->getClientOriginalExtension();
 
+            Image::make($avatar)->resize(300, 300)->save(public_path('uploads/avatar/'. $filename));
+
+            $user = Auth::user();
+            $user->picture = $filename;
+            $user->save();
+        }
+        return view('user.settings');
     }
 }
