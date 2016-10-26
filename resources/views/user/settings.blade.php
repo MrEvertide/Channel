600b3b2e7cd0e13b98632b5763997938965c0ff0
Cédric Nolin <ced.star.002@gmail.com>
2016-10-26 17:07:13 -0400

    change capitilization for the user name in the settings page

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-10 col-md-offset-1">
             <img src="/uploads/avatar/{{Auth::user()->picture}}" alt="profile picture" class="settings-avatar-image">
             <div class="profile-image">
-                <h2>{{Auth::user()->name}}'s profile</h2>
+                <h2>{{ucfirst(Auth::user()->name)}}'s Profile</h2>
                 <form enctype="multipart/form-data" action="/settings" method="POST" class="avatar-form">
                     <label for="avatar">Change your avatar</label>
                     <input type="file" name="avatar">
