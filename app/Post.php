63a2e9c721109ff096bb923d28a21ec0e786388f
Cédric Nolin <ced.star.002@gmail.com>
2016-10-28 19:45:26 -0400

    add methods to parse the dates used and return readable formats

--- app/Post.php
+++ app/Post.php
@@ -3,10 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon as Carbon;
 
 class Post extends Model
 {
-    protected $fillable = ["content", "user_id"];
+    protected $fillable = ["content", "user_id", "created_at", "updated_at"];
 
 
     /**
@@ -16,5 +17,30 @@ class Post extends Model
      */
     public function user() {
         return $this->belongsTo('App\User');
+    }
+
+
+    /**
+     * Method used to parse the date such as created_at and updated_at to a readable format
+     *
+     * @param $date
+     * @return string
+     */
+    public function parseDate($date) {
+        $parseDate = Carbon::createFromFormat('Y-m-d H:i:s', $date);
+        return $parseDate->format('l jS F Y h:i A');
+
+    }
+
+    /**
+     * Method used to parse the date such as created_at and updated_at to a readable format - Returns a shorter and less
+     * precise date
+     *
+     * @param $date
+     * @return string
+     */
+    public function parseDateShort($date) {
+        $parseDate = Carbon::createFromFormat('Y-m-d H:i:s', $date);
+        return $parseDate->format('l jS F');
     }
 }
