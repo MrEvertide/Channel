8f0eae7d81f7e5ba4a432c768f69b9a9b5370d31
Cédric Nolin <ced.star.002@gmail.com>
2016-10-27 23:25:57 -0400

    add a post model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ["content", "user_id"];
+
+
+    /**
+     * Add the User relationship to the post's Model
+     *
+     * @return mixed
+     */
+    public function user() {
+        $this->belongsTo('App\User');
+    }
 }
